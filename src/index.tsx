e457ce0a1198cc3387d6bb5a9376b3729a6ef902
Alex Dev Sm <leshasmlesha@gmail.com>
2022-03-18 17:36:01 +0300

    Добавил тему

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,32 @@ import App from "./document";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { ruRU } from "@mui/material/locale";
 import { ruRU as ruRUgrid } from "@mui/x-data-grid-pro";
-import '@fontsource/roboto/500.css';
-const theme = createTheme({}, ruRU, ruRUgrid);
+import "@fontsource/roboto/500.css";
+const theme = createTheme(
+  {
+    typography: {
+      fontSize: 13,
+    },
+    components: {
+      // Name of the component
+      MuiTextField: {
+        defaultProps: { size: "small" },
+      },
+      MuiAutocomplete: {
+        defaultProps: { size: "small" },
+      },
+      MuiFormControl: {
+        defaultProps: { size: "small" },
+      },
+      //@ts-ignore
+      MuiDataGrid: {
+        defaultProps: { density: "compact" },
+      },
+    },
+  },
+  ruRU,
+  ruRUgrid
+);
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
