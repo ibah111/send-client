26e8381c7792b58c7d89a9ca1b2192d635d8f0c3
Alex Dev Sm <leshasmlesha@gmail.com>
2022-04-13 16:08:29 +0300

    Обновил пакеты

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./document";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { ruRU } from "@mui/material/locale";
 import { ruRU as ruRUgrid } from "@mui/x-data-grid-pro";
 import "@fontsource/roboto/500.css";
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 const theme = createTheme(
   {
     typography: {
@@ -30,11 +33,10 @@ const theme = createTheme(
   ruRU,
   ruRUgrid
 );
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
