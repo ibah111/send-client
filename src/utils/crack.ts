a8127504dc898da5021527412356f1f8c0bcf372
Alex Dev Sm <leshasmlesha@gmail.com>
2022-05-17 13:09:01 +0300

    Поправил генератор

--- src/utils/crack.ts
+++ src/utils/crack.ts
@@ -1,8 +1,8 @@
-import {generateLicence, LicenseInfo} from '@mui/x-license-pro';
+import {generateLicense, LicenseInfo} from '@mui/x-license-pro';
 export default function license() {
     const oneDayInMS = 1000 * 60 * 60 * 24;
 const oneYear = oneDayInMS * 365;
-const validLicense = generateLicence({
+const validLicense = generateLicense({
     expiryDate: new Date(new Date().getTime() + oneYear),
     orderNumber: 'MUI-45675676',
   });
