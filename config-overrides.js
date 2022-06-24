1d85b7cf169411137a6a7384f80c0eca24f9149d
Alex Dev Sm <leshasmlesha@gmail.com>
2022-06-24 15:19:52 +0300

    Сделал автоопределение

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,19 @@
 const path = require("path");
+const fs = require("fs").promises;
+const s = require("semver");
+const gitSemverTags = require("git-semver-tags");
+const gitGet = () =>
+  new Promise((resolve) => {
+    gitSemverTags({ tagPrefix: "v" }, (err, result) => {
+      const tags = result.map((value) => s.clean(value));
+      resolve(tags[0]);
+    });
+  });
+const prepare = async () => {
+  const tag = await gitGet();
+  await fs.writeFile("./src/utils/.version.js", `module.exports = "${tag}";`);
+};
+prepare();
 module.exports = {
   webpack: function (config, env) {
     if (env === "production") {
