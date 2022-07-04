e8fa18586aa8b55da1be984697889ca15b74f1a7
Alex Dev Sm <leshasmlesha@gmail.com>
2022-07-04 10:47:31 +0300

    Поправил ошибку

--- src/api/createExec.ts
+++ src/api/createExec.ts
@@ -1,7 +1,13 @@
 import store from "../Reducer";
 import axios from "axios";
 import server from "../utils/server";
-export default async function createExec(value: number, old: any = {}) {
+export class CreateExecOld {
+  court_doc_num: string;
+  executive_typ: number;
+  court_date: Date;
+  entry_force_dt: Date;
+}
+export default async function createExec(value: number, old?: CreateExecOld) {
   const token = store.getState().User.token;
   const response = await axios.post<number | false>(server() + "/create_exec", {
     token,
