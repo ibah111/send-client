7ac30d3961af3b055222e0b1ef6f793aacb5aa68
Alex Dev Sm <leshasmlesha@gmail.com>
2022-07-21 12:11:05 +0300

    Поменял тип

--- src/api/updateDebt.ts
+++ src/api/updateDebt.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import server from "../utils/server";
 export default async function updateExec(law_exec_id: number, debt_id: number) {
   const token = store.getState().User.token;
-  const response = await axios.post(server() + "/update_debt", {
+  const response = await axios.post<boolean>(server() + "/update_debt", {
     token,
     law_exec_id,
     debt_id,
