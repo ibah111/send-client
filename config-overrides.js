5d7781aef8a2b8e5d40d855ccb3549519eacd96f
Alex Dev Sm <leshasmlesha@gmail.com>
2023-02-21 10:02:58 +0300

    Убрал php

--- config-overrides.js
+++ config-overrides.js
@@ -16,30 +16,9 @@ const prepare = async () => {
 prepare();
 module.exports = {
   webpack: function (config, env) {
-    if (env === 'production') {
-      config.plugins.find((value) => value.userOptions).userOptions.filename =
-        'index.php';
-      config.module.rules
-        .find((value) => value.oneOf)
-        .oneOf.find((value) => value.type === 'asset/resource')
-        .exclude.push(/\.php$/);
-    }
     const fallback = config.resolve.fallback || {};
     Object.assign(fallback, { path: require.resolve('path-browserify') });
     config.resolve.fallback = fallback;
     return config;
-  },
-  paths: (paths, env) => {
-    switch (env) {
-      case 'production':
-        paths.appPublic = path.resolve(__dirname, 'public/prod');
-        paths.appHtml = path.resolve(__dirname, 'public/prod/index.php');
-        break;
-      case 'development':
-        paths.appPublic = path.resolve(__dirname, 'public/dev');
-        paths.appHtml = path.resolve(__dirname, 'public/dev/index.html');
-        break;
-    }
-    return paths;
   },
 };
