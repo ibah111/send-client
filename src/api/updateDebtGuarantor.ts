5aaf678e10f5cf4a523a0ee2fa249ec23e79fbb6
Alex Dev Sm <leshasmlesha@gmail.com>
2023-07-12 09:34:55 +0300

    fix(debt-guarantor): исправил адрес отправки

--- src/api/updateDebtGuarantor.ts
+++ src/api/updateDebtGuarantor.ts
@@ -1,7 +1,5 @@
 import { DebtGuarantor } from '@contact/models';
 import { CreationAttributes } from '@sql-tools/sequelize';
-import { plainToInstance } from 'class-transformer';
-import { validateOrReject } from 'class-validator';
 import { createDebtGuarantorInstance } from '../pages/send/user/Components/DebtGuarantor/DebtGuarantorInstance';
 import requests from '../utils/requests';
 import { of } from 'rxjs';
@@ -14,24 +12,15 @@ import {
 import { transformError } from '../utils/processError';
 
 const DebtGuarantorInstance = createDebtGuarantorInstance();
-export async function call(body: CreationAttributes<DebtGuarantor>) {
-  const data = plainToInstance(DebtGuarantorInstance, body);
-  await validateOrReject(data);
-  if (body === null) {
-    throw new Error('Данные пусты');
-  }
-  const res = await requests.post<DebtGuarantor | { update: true }>(
-    '/create_or_update_debt_guarantor',
-    data,
-  );
-  return res;
-}
 export default function updateDebtGuarantor(
   body: CreationAttributes<DebtGuarantor>,
 ) {
   return of(body).pipe(
     validateData(DebtGuarantorInstance, { resultTransform: true }),
-    post<DebtGuarantor | { update: true }>(requests, '/debt_calc'),
+    post<DebtGuarantor | { update: true }>(
+      requests,
+      '/create_or_update_debt_guarantor',
+    ),
     transformAxios(),
     transformError('debt_guarantor'),
     authRetry(),
