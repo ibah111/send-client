222fef62d16cb3529ae8ebd1e88db941157d4872
Ibah <starlord77@mail.ru>
2024-05-24 15:24:02 +0300

    feat: marked prod or dev version on web client

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,12 +1,15 @@
 import config from '../config/server.json';
+
+//@ts-ignore
+export const env: 'prod' | 'dev' = process.env.NODE_ENV;
+
 export default function server(name?: string) {
+  const server = env === 'dev' ? config.server : config.server_dev;
   switch (name) {
     case 'bitrix':
     case 'oauth':
       return config[name];
     default:
-      return process.env.NODE_ENV === 'prod'
-        ? config.server
-        : config.server_dev;
+      return server;
   }
 }
