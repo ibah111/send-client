8d5fa93215dce84dbe0d0a30205f9d30f978950c
Ibah <starlord77@mail.ru>
2024-05-24 15:28:16 +0300

    feat: add env to scripts + fixed server url depends from node variable

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -4,7 +4,7 @@ import config from '../config/server.json';
 export const env: 'prod' | 'dev' = process.env.NODE_ENV;
 
 export default function server(name?: string) {
-  const server = env === 'dev' ? config.server : config.server_dev;
+  const server = env === 'prod' ? config.server : config.server_dev;
   switch (name) {
     case 'bitrix':
     case 'oauth':
