a712f7cd5a04448fcab02202e8cdaf29b07f7109
Ibah <starlord77@mail.ru>
2024-07-26 20:25:32 +0300

    feat: component

--- src/pages/send/user/Components/Up/Send/Section5/RequisitesForm/RequisitsDialogs/AddPortfolioToRequisitsDialog.tsx
+++ src/pages/send/user/Components/Up/Send/Section5/RequisitesForm/RequisitsDialogs/AddPortfolioToRequisitsDialog.tsx
@@ -1,6 +1,8 @@
+import { Portfolio } from '@contact/models';
 import { Dialog, DialogContent, DialogTitle } from '@mui/material';
-import { DataGridPremium } from '@mui/x-data-grid-premium';
+import { DataGridPremium, GridColDef } from '@mui/x-data-grid-premium';
 import React from 'react';
+import getAllLinksByRequisites from '../../../../../../../../../api/PortfoliosToRequisites/getAllLinksByRequisites';
 
 interface DialogProps {
   open: boolean;
@@ -8,21 +10,60 @@ interface DialogProps {
   onClose: () => void;
 }
 
-export default function AddPortfolioToRequisits({
+export default function PortfoliosToRequisits({
   id,
   onClose,
   open,
 }: DialogProps) {
-  const [rows, setRows] = React.useState([]);
-  React.useEffect(() => {});
+  const [rows, setRows] = React.useState<Portfolio[]>([]);
+  const columns = PortfolioToRequisitesColumns();
+  React.useEffect(() => {
+    getAllLinksByRequisites(id).subscribe((result) => setRows(result));
+  }, [id, rows]);
   return (
     <>
       <Dialog open={open} onClose={onClose}>
         <DialogTitle></DialogTitle>
         <DialogContent>
-          <DataGridPremium columns={[]} rows={rows} />
+          <DataGridPremium columns={columns} rows={rows} />
         </DialogContent>
       </Dialog>
     </>
   );
+}
+
+function PortfolioToRequisitesColumns() {
+  const columns: GridColDef<Portfolio>[] = [
+    {
+      field: 'id',
+    },
+    {
+      field: 'name',
+    },
+    {
+      field: 'sign_date',
+    },
+    {
+      type: 'number',
+      field: 'bank.id',
+      valueGetter(params) {
+        return params.row.Bank?.id;
+      },
+    },
+    {
+      type: 'string',
+      field: 'bank.name',
+      valueGetter(params) {
+        return params.row.Bank?.name;
+      },
+    },
+    {
+      type: 'string',
+      field: 'bank.full_name',
+      valueGetter(params) {
+        return params.row.Bank?.full_name;
+      },
+    },
+  ];
+  return columns;
 }
