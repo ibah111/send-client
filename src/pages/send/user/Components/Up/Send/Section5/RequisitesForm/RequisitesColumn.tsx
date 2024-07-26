adc154c6a164450c97557319e3efb46b989f758f
Ibah <starlord77@mail.ru>
2024-07-26 17:44:46 +0300

    feat: колонки таблицы реквизитов

---
+++ src/pages/send/user/Components/Up/Send/Section5/RequisitesForm/RequisitesColumn.tsx
@@ -0,0 +1,122 @@
+import { GridActionsCellItem, GridColDef } from '@mui/x-data-grid-premium';
+import { BankRequisitesClass } from '../../../../../../../../api/BankRequisites/BankRequisitesInput';
+import {
+  SectionFiveEventDialog,
+  SectionFiveEvents,
+} from './RequisitesTableDialog';
+import AddIcon from '@mui/icons-material/Add';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import { Tooltip } from '@mui/material';
+
+interface requisitsColumnsProps {
+  DialogTarget: EventTarget;
+}
+
+export default function RequisitsColumns({
+  DialogTarget,
+}: requisitsColumnsProps): GridColDef<BankRequisitesClass>[] {
+  const columns: GridColDef<BankRequisitesClass>[] = [
+    {
+      field: 'id',
+      headerName: 'ID',
+    },
+    {
+      field: 'name',
+      headerName: 'Название',
+    },
+    {
+      field: 'recipient',
+      headerName: 'Получатель',
+    },
+    {
+      field: 'br_name',
+      headerName: 'Банк получателя',
+    },
+    {
+      field: 'inn',
+      headerName: 'ИНН',
+    },
+    {
+      field: 'kpp',
+      headerName: 'КПП',
+    },
+    {
+      field: 'r_account',
+      headerName: 'Рассчётный счёт',
+    },
+    {
+      field: 'k_account',
+      headerName: 'Корр.счёт',
+    },
+    {
+      field: 'bik',
+      headerName: 'БИК',
+    },
+    {
+      field: 'pay_purpose',
+      headerName: 'Назначение платежа / ОГРНИП',
+    },
+    {
+      field: 'typ',
+      headerName: 'Тип реквизита',
+    },
+    {
+      headerName: 'Действия',
+      field: 'actions',
+      type: 'actions',
+      getActions: (params) => [
+        <>
+          <>
+            <GridActionsCellItem
+              icon={<AddIcon />}
+              label="Open"
+              onClick={() => {
+                const rowValue = params.row.id;
+                DialogTarget.dispatchEvent(
+                  new SectionFiveEventDialog(
+                    SectionFiveEvents.openRequisitesAddPortfoliosToRequisitesDialog,
+                    rowValue,
+                  ),
+                );
+              }}
+            />
+          </>
+        </>,
+        <>
+          <Tooltip title={'Редактировать реквизит'}>
+            <GridActionsCellItem
+              onClick={() => {
+                const rowValue = params.row.id;
+                DialogTarget.dispatchEvent(
+                  new SectionFiveEventDialog(
+                    SectionFiveEvents.openRequisitesEditDialog,
+                    rowValue,
+                  ),
+                );
+              }}
+              icon={<ModeEditIcon />}
+              label={'Edit'}
+            />
+          </Tooltip>
+        </>,
+      ],
+    },
+    {
+      field: 'kbe',
+      headerName: 'Unknown (kbe)',
+    },
+    {
+      field: 'knp',
+      headerName: 'Unknown (knp)',
+    },
+    {
+      field: 'kod',
+      headerName: 'Unkown (kod)',
+    },
+  ];
+  return columns.map((items) => ({
+    ...items,
+    headerAlign: 'center',
+    align: 'center',
+  }));
+}
