82b0c8c4f59a6b7d67b2d6d327241ee306644387
Ibah <starlord77@mail.ru>
2024-07-30 15:57:13 +0300

    feat: поправил апи

--- src/api/PortfoliosToRequisites/getAllPortfolio.ts
+++ src/api/PortfoliosToRequisites/getAllPortfolio.ts
@@ -1,15 +1,21 @@
 import { Portfolio } from '@contact/models';
-import { authRetry, get, transformAxios } from '@tools/rxjs-pipes';
+import { authRetry, post, transformAxios } from '@tools/rxjs-pipes';
 import { forkJoin, of } from 'rxjs';
 import requests from '../../utils/requests';
 import { transformError } from '../../utils/processError';
 
-interface searchProps {}
+class searchProps {
+  name: string;
+  paginationModel: {
+    page: number;
+    pageSize: number;
+  };
+}
 
-export default function getAllPortfolio() {
-  const url = of(`/PortfoliosToRequisites/getAllPortfolio`);
-  return forkJoin([requests, url]).pipe(
-    get<Portfolio[]>(),
+export default function getAllPortfolio(body: searchProps) {
+  const url = of(`/BankRequisites/getAllPortfolio`);
+  return forkJoin([requests, url, of(body)]).pipe(
+    post<Portfolio[]>(),
     transformAxios(),
     transformError(),
     authRetry(),
