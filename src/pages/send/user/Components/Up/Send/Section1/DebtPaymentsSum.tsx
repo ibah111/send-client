4a1a1f330966c61deac4477e99a3d3ca46c6b1d1
Ibah <starlord77@mail.ru>
2024-09-04 15:33:39 +0300

    fix: фикс отправки

--- src/pages/send/user/Components/Up/Send/Section1/DebtPaymentsSum.tsx
+++ src/pages/send/user/Components/Up/Send/Section1/DebtPaymentsSum.tsx
@@ -3,7 +3,7 @@ import getData from '../../../../../../../utils/getData';
 import { NumberFormatCustom } from './TotalSum';
 
 export default function DebtPaymentsSum() {
-  const data = getData('debt_payments_sum');
+  const data = getData('debt_payments_sum', 'number', true, true);
   return (
     <>
       <Grid item>
--- src/pages/send/user/Components/Up/Send/Section1/TotalSum.tsx
+++ src/pages/send/user/Components/Up/Send/Section1/TotalSum.tsx
@@ -45,9 +45,14 @@ export default function TotalSum() {
   const { t } = useTranslation();
   const total_sum_data = getData('total_sum', 'number', true);
 
-  const court_sum_data = getData('court_sum', 'number');
+  const court_sum_data = getData('court_sum', 'number', true, true);
   const court_sum_value = court_sum_data.value;
-  const debt_payments_sum_data = getData('debt_payments_sum', 'number');
+  const debt_payments_sum_data = getData(
+    'debt_payments_sum',
+    'number',
+    true,
+    true,
+  );
   const debt_payments_sum_value = debt_payments_sum_data.value;
   React.useEffect(() => {
     if (court_sum_value && debt_payments_sum_value) {
