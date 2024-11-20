a4a7b936690d626024736309f3c23a0eb71bd36d
Ibah <starlord77@mail.ru>
2024-11-20 11:32:59 +0300

    feat: precision function

--- src/api/updateExec.ts
+++ src/api/updateExec.ts
@@ -9,10 +9,26 @@ type FileUpdate =
       file: { data: number[] };
       name: string;
     };
+
+function getPrecision(num: number): number {
+  const value = (Math.round(num * 100) / 100).toFixed(2);
+  return Number(value);
+}
+
 export default function updateExec() {
   const url = of('/update_exec');
   const storeState = store.getState().Send;
-  const data = defer(() => of({ ...storeState, options: { save_file: true } }));
+  const data = defer(() =>
+    of({
+      ...storeState,
+      court_sum: getPrecision(store.getState().Send.court_sum as number),
+      debt_payments_sum: getPrecision(
+        store.getState().Send.debt_payments_sum as number,
+      ),
+      total_sum: getPrecision(store.getState().Send.total_sum as number),
+      options: { save_file: true },
+    }),
+  );
   return forkJoin([requests, url, data]).pipe(
     post<FileUpdate>(),
     transformAxios(),
