9c732c6d34b1ff1d3474d0d41bbbeef8abc6b32e
ibah <starlord77@mail.ru>
2025-03-06 11:14:21 +0300

    feat: fix names

--- src/api/Links/addLink.ts
+++ src/api/Links/addLink.ts
@@ -3,7 +3,7 @@ import { authRetry, post, transformAxios } from '@tools/rxjs-pipes';
 import { transformError } from '../../utils/processError';
 import requests from '../../utils/requests';
 
-export default function addLink(data: { linkName: string; linkUrl: string }) {
+export default function addLink(data: { item_name: string; item_url: string }) {
   const url = of('/Links/addLink');
   return forkJoin([requests, url, of(data)]).pipe(
     post<boolean>(),
