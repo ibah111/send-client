5883206581dba236bc8300a558e876227de431a2
ibah <starlord77@mail.ru>
2025-03-06 11:14:28 +0300

    feat: removed async

--- src/api/Links/deleteLink.ts
+++ src/api/Links/deleteLink.ts
@@ -4,7 +4,7 @@ import { authRetry, post, transformAxios } from '@tools/rxjs-pipes';
 import { transformError } from '../../utils/processError';
 import requests from '../../utils/requests';
 
-export default async function deleteLink(data: LinkType) {
+export default function deleteLink(data: LinkType) {
   const url = of('/Links/deleteLink');
   return forkJoin([requests, url, of(data)]).pipe(
     post<boolean>(),
