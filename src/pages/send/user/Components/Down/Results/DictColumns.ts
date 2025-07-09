5aedc0616c6b8ef42f76a3cf7497d65604f4c77c
ibah <starlord77@mail.ru>
2025-07-09 20:52:56 +0300

    feat: dict columns

---
+++ src/pages/send/user/Components/Down/Results/DictColumns.ts
@@ -0,0 +1,8 @@
+import { GridColDef } from '@mui/x-data-grid-premium';
+
+export const DictColumns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 100 },
+  { field: 'parent_id', headerName: 'Родитель', width: 100 },
+  { field: 'code', headerName: 'Код', width: 100 },
+  { field: 'name', headerName: 'Наименование', width: 300 },
+];
